3290988fb7ff7b22c8fd0f48e5769fffbbcf0aea
Max <mkorobov2002@mail.ru>
2022-11-04 23:19:21 +0300

    ArchivePage Created

---
+++ pages/archive/index.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import Header from '../../components/main/Header';
+
+type Props = {};
+
+export default function ArchivePage({}: Props) {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+}
