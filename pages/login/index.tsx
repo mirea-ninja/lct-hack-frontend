91df740bb5061942538d6c68bb04e8548a364316
Max <mkorobov2002@mail.ru>
2022-11-04 21:41:41 +0300

    LoginPage Stylized

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { FormControlLabel, TextField } from '@mui/material';
 import { Button } from '@mui/material';
 import { Link } from '@mui/material';
 import { Checkbox } from '@mui/material';
@@ -11,6 +11,7 @@ import AppButton from '../../components/buttons/AppButton';
 import GosuslugiIcon from '../../components/icons/GosuslugiIcon';
 import ACMLogoIcon from '../../components/icons/ACMLogoIcon';
 import NinjaIcon from '../../components/icons/NinjaIcon';
+import AppCheckbox from '../../components/checkboxes/AppCheckbox';
 import styles from '../../styles/Login.module.scss';
 
 type Props = {};
@@ -73,64 +74,93 @@ function RightSide(props: StackProps) {
         backgroundColor: 'var(--bg-clr-pure-white)',
       }}>
       <Stack sx={{ maxWidth: '445px', flex: 1, width: '100%' }}>
-        <Typography
-          variant='h4'
-          sx={{
-            marginBottom: '50px',
-            fontWeight: 700,
-            fontSize: '55px',
-            lineHeight: '60px',
-            color: 'var(--text-clr-main)',
-          }}>
-          Вход
-        </Typography>
-        <Stack sx={{ gap: '20px' }}>
-          <Stack sx={{ gap: '10px' }}>
-            <TextField id='outlined-read-only-input' placeholder='Логин' />
-            <TextField id='outlined-read-only-input' placeholder='Пароль' />
-          </Stack>
-          <Stack
-            direction='row'
-            justifyContent='space-between'
-            alignItems='center'>
-            <Stack direction='row' alignItems='center'>
-              <Checkbox sx={{ padding: 0 }}></Checkbox>
-              <Typography paddingLeft={2}>Сохранить данные</Typography>
-            </Stack>
-            <Link>Восстановить пароль</Link>
-          </Stack>
-        </Stack>
-
-        <Stack sx={{ gap: '30px', marginTop: '40px' }}>
-          <AppButton>Войти</AppButton>
+        <form>
           <Typography
-            align='center'
+            variant='h4'
             sx={{
-              fontWeight: 500,
-              fontSize: '16px',
+              marginBottom: '50px',
+              fontWeight: 700,
+              fontSize: '55px',
+              lineHeight: '60px',
+              color: 'var(--text-clr-main)',
             }}>
-            ИЛИ
+            Вход
           </Typography>
           <Stack sx={{ gap: '20px' }}>
-            <AppButton variant='secondary'>Войти через SSO</AppButton>
+            <Stack sx={{ gap: '10px' }}>
+              <TextField
+                id='outlined-read-only-input'
+                placeholder='Логин'
+                name='login'
+              />
+              <TextField
+                id='outlined-read-only-input'
+                placeholder='Пароль'
+                name='password'
+              />
+            </Stack>
             <Stack
               direction='row'
               justifyContent='space-between'
-              sx={{
-                gap: '20px',
-              }}>
-              <AppButton
-                sx={{ flex: 1 }}
-                variant='secondary'
-                startIcon={<GosuslugiIcon />}>
-                Госуслуги
-              </AppButton>
-              <AppButton sx={{ flex: 1 }} variant='secondary'>
-                Mos.ru
-              </AppButton>
+              alignItems='center'>
+              <Stack direction='row' alignItems='center'>
+                <FormControlLabel
+                  control={<AppCheckbox defaultChecked />}
+                  label='Сохранить данные'
+                  sx={{
+                    marginLeft: 0,
+                  }}
+                />
+              </Stack>
+              <Link
+                href=''
+                sx={{
+                  fontFamily: 'var(--font-primary)',
+                  textDecoration: 'none',
+                  color: 'var(--accent-clr)',
+                  fontWeight: 700,
+                  fontSize: '16px',
+
+                  '&:hover': {
+                    color: 'var(--accent-clr-hover)',
+                  },
+                }}>
+                Восстановить пароль
+              </Link>
             </Stack>
           </Stack>
-        </Stack>
+
+          <Stack sx={{ gap: '30px', marginTop: '40px' }}>
+            <AppButton type='submit'>Войти</AppButton>
+            <Typography
+              align='center'
+              sx={{
+                fontWeight: 500,
+                fontSize: '16px',
+              }}>
+              ИЛИ
+            </Typography>
+            <Stack sx={{ gap: '20px' }}>
+              <AppButton variant='secondary'>Войти через SSO</AppButton>
+              <Stack
+                direction='row'
+                justifyContent='space-between'
+                sx={{
+                  gap: '20px',
+                }}>
+                <AppButton
+                  sx={{ flex: 1 }}
+                  variant='secondary'
+                  startIcon={<GosuslugiIcon />}>
+                  Госуслуги
+                </AppButton>
+                <AppButton sx={{ flex: 1 }} variant='secondary'>
+                  Mos.ru
+                </AppButton>
+              </Stack>
+            </Stack>
+          </Stack>
+        </form>
       </Stack>
     </Box>
   );
