5bab2aaa953cdbc4a6ac5383f2902886971e30ab
Max <mkorobov2002@mail.ru>
2022-11-06 03:29:24 +0300

    SuccessModal Imported

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import BasicModal from '../components/modals/BasicModal';
 import DisconnectModal from '../components/modals/DisconnectModal';
+import SuccessModal from '../components/modals/SuccessModal';
 
 const TestPage = () => {
   const [open, setOpen] = React.useState(false);
@@ -11,7 +11,8 @@ const TestPage = () => {
   return (
     <div>
       <Button onClick={handleOpen}>Open modal</Button>
-      <DisconnectModal open={open} onClose={handleClose} />
+      {/* <DisconnectModal open={open} onClose={handleClose} /> */}
+      <SuccessModal open={open} onClose={handleClose} />
     </div>
   );
 };
