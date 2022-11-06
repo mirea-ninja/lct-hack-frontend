bc37d2e4ef14ca87888a0bd6564e9de06889344b
Max <mkorobov2002@mail.ru>
2022-11-06 04:01:58 +0300

    NoResultItem Deleted

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import DisconnectModal from '../components/modals/DisconnectModal';
 import SuccessModal from '../components/modals/SuccessModal';
-import NoResultItem from '../components/items/NoResultItem';
 
 const TestPage = () => {
   const [open, setOpen] = React.useState(false);
@@ -14,8 +13,6 @@ const TestPage = () => {
       <Button onClick={handleOpen}>Open modal</Button>
       {/* <DisconnectModal open={open} onClose={handleClose} /> */}
       <SuccessModal open={open} onClose={handleClose} />
-      <br />
-      <NoResultItem />
     </div>
   );
 };
