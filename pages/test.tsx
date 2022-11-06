86490d9f8e0c7246bbb736943ac2e47913496c13
Max <mkorobov2002@mail.ru>
2022-11-06 03:57:35 +0300

    NoResultItem Imported

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import DisconnectModal from '../components/modals/DisconnectModal';
 import SuccessModal from '../components/modals/SuccessModal';
+import NoResultItem from '../components/items/NoResultItem';
 
 const TestPage = () => {
   const [open, setOpen] = React.useState(false);
@@ -13,6 +14,8 @@ const TestPage = () => {
       <Button onClick={handleOpen}>Open modal</Button>
       {/* <DisconnectModal open={open} onClose={handleClose} /> */}
       <SuccessModal open={open} onClose={handleClose} />
+      <br />
+      <NoResultItem />
     </div>
   );
 };
